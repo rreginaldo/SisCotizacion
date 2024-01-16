09ce5a187bd1e4ffeb11ddeac554ea058413fd21
RobiR1709 <robertodrm10@gmail.com>
2024-01-16 08:33:56 -0500

    Subir proyecto 1ra vez

---
+++ SoftkitWeb/Controllers/CotizadorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoftkitWeb.Controllers
+{
+    public class CotizadorController : Controller
+    {
+        private readonly ILogger<CotizadorController> _logger;
+
+        public CotizadorController(ILogger<CotizadorController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SoftkitWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SoftkitWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
